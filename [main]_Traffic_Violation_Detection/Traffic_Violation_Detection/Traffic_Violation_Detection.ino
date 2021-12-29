c4a2539db829f6a3b3d3137d91ee3c62eca06c9f
gremlinflat_ <fahri.119140205@student.itera.ac.id>
2021-12-30 01:39:16 +0700

    add(arduinofiles) : creating a boilerplate, header, and basic setup configuration on arduino files

---
+++ [main]_Traffic_Violation_Detection/Traffic_Violation_Detection/Traffic_Violation_Detection.ino
@@ -0,0 +1,213 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+HTTPClient http;
+#include "esp_camera.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_http_server.h"
+#include "esp_camera.h"
+#include "img_converters.h"
+#include <Preferences.h>
+Preferences preferences;
+
+// image transfering data structure
+typedef struct {
+        httpd_req_t *req;
+        size_t len;
+} jpg_chunking_t;
+
+// --------------Streaming configuration---------------
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+httpd_handle_t stream_httpd = NULL;
+httpd_handle_t camera_httpd = NULL;
+// ----------------------------------------------------
+
+// --------------GPIO PIN configuration----------------
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+// ----------------------------------------------------
+
+// --------------State Definition-----------------------
+byte ReceiveState=0;
+byte cmdState=1;
+byte strState=1;
+byte questionstate=0;
+byte equalstate=0;
+byte semicolonstate=0;
+// ----------------------------------------------------
+
+//default Wifi credentials
+String wifi_ssid ="FireShark 776t";
+String wifi_password ="123456789";
+
+// Access Point configuration
+const char* apssid = "Smart_Traffic 18";
+const char* appassword = "123456789";
+
+void setup() {
+  //to-do : fail-safe ketika arus tidak stabil
+  
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  // --------------ESP CAM Configuration-----------------------
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  // ----------------------------------------------------
+
+  //---------------------------------------------------------------------------
+  // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+  //            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+  //            Partial images will be transmitted if image exceeds buffer size
+  //   
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  //----------------------------------------------------------------------------
+
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  //Camera initialization
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
+
+  //--------------------Default video configuration----------------------
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  
+  //drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_QVGA);
+  s->set_hmirror(s, 1);
+
+  ledcAttachPin(4, 4);  
+  ledcSetup(4, 5000, 8);    
+  //----------------------------------------------------------------------------
+
+  //--------------------------------Wifi & AP INIT--------------------------------
+  WiFi.mode(WIFI_AP_STA);
+
+  Serial.println();
+
+  //Preferences_write("wifi", "ssid", "");
+  //Preferences_write("wifi", "password", "");
+        
+  wifi_ssid = Preferences_read("wifi", "ssid");
+  wifi_password = Preferences_read("wifi", "password");
+
+  if (wifi_ssid!="") {
+    for (int i=0;i<2;i++) {
+      WiFi.begin(wifi_ssid.c_str(), wifi_password.c_str());]
+    
+      delay(1000);
+      Serial.println("");
+      Serial.print("Connecting to ");
+      Serial.println(wifi_ssid);
+      
+      long int StartTime=millis();
+      while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          if ((StartTime+5000) < millis()) break;]
+      } 
+    
+      if (WiFi.status() == WL_CONNECTED) {]
+        WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);]示客戶端IP         
+        Serial.println("");
+        Serial.println("STAIP address: ");
+        Serial.println(WiFi.localIP());
+        Serial.println("");
+    
+        for (int i=0;i<5;i++) {]
+          ledcWrite(4,10);
+          delay(200);
+          ledcWrite(4,0);
+          delay(200);    
+        }
+
+        // line notify API TOKEN
+        if (LineToken != "") 
+          LineNotify_http_get(LineToken, WiFi.localIP().toString());
+        break;
+      }
+    } 
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.softAP((WiFi.softAPIP().toString()+"_"+(String)apssid).c_str(), appassword);         
+
+  // flashing led 2 times
+    for (int i=0;i<2;i++) {
+      ledcWrite(4,10);
+      delay(1000);
+      ledcWrite(4,0);
+      delay(1000);    
+    }
+  }  
+
+  Serial.println();
+  Serial.println("Access Point IP address: ");
+  Serial.println(WiFi.softAPIP()); 
+  Serial.println();
+  startCameraServer(); 
+
+  // turning on LED as indicator
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);      
+}
+
+void loop {
+    
+}
